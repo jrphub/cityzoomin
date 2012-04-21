af71b047b1427b5f1606dd3433e0308272f37dce
Jyoti Ranjan Pattnaik <jyotiranjanpattnaik@gmail.com>
2012-04-22 02:32:32 +0530

    Trying to get missing js file in heroku

--- config/application.rb
+++ config/application.rb
@@ -38,6 +38,7 @@ module Cityzoomin
 
     # Configure sensitive parameters which will be filtered from the log file.
     config.filter_parameters += [:password]
+    config.assets.precompile << '*.js'
 
     # Use SQL instead of Active Record's schema dumper when creating the database.
     # This is necessary if your schema can't be completely dumped by the schema dumper,

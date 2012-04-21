de5f990403b603cf08f0e43e29b75cdf51bfc4ea
Jyoti Ranjan Pattnaik <jyotiranjanpattnaik@gmail.com>
2012-04-22 02:41:30 +0530

    js files found, but now css files are missing, solving that

--- config/application.rb
+++ config/application.rb
@@ -39,6 +39,7 @@ module Cityzoomin
     # Configure sensitive parameters which will be filtered from the log file.
     config.filter_parameters += [:password]
     config.assets.precompile << '*.js'
+    config.assets.precompile << '*.css'
 
     # Use SQL instead of Active Record's schema dumper when creating the database.
     # This is necessary if your schema can't be completely dumped by the schema dumper,

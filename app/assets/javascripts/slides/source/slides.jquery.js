1663324ceb896993de4edf836fab5827fff6841d
Jyoti Ranjan Pattnaik <jyotiranjanpattnaik@gmail.com>
2012-04-28 11:22:00 +0530

    missing files twitter-quotes.png and loading.gif is added

--- app/assets/javascripts/slides/source/slides.jquery.js
+++ app/assets/javascripts/slides/source/slides.jquery.js
@@ -485,7 +485,7 @@
 	// default options
 	$.fn.slides.option = {
 		preload: false, // boolean, Set true to preload images in an image based slideshow
-		preloadImage: '/img/loading.gif', // string, Name and location of loading image for preloader. Default is "/img/loading.gif"
+		preloadImage: '/assets/loading.gif', // string, Name and location of loading image for preloader. Default is "/assets/loading.gif"
 		container: 'slides_container', // string, Class name for slides container. Default is "slides_container"
 		generateNextPrev: false, // boolean, Auto generate next/prev buttons
 		next: 'next', // string, Class name for next button

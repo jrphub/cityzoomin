0737762da0352c1964c64b1c18311c15af91b776
Jyoti Ranjan Pattnaik <jyotiranjanpattnaik@gmail.com>
2012-05-05 21:49:49 +0530

    security holes regarding unsigned user is removed and friendly forwarding is done

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,8 @@
 class UsersController < ApplicationController
+  
+  before_filter :signed_in_user, only: [:edit, :update]
+  before_filter :correct_user,   only: [:edit, :update]
+  
   def show
     @user = User.find(params[:id])
   end
@@ -31,4 +35,18 @@ class UsersController < ApplicationController
       render "edit"
     end
   end
+  
+  private
+
+    def signed_in_user
+      unless signed_in?
+        store_location
+        redirect_to signin_path, notice: "Please sign in."
+      end
+    end
+
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_path) unless current_user?(@user)
+    end
 end

f08c9d548a12cd43b22cb67dc679afc54bfb3c00
Jyoti Ranjan Pattnaik <jyotiranjanpattnaik@gmail.com>
2012-05-05 14:10:52 +0530

    user edit page formed

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,9 +2,11 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
+  
   def new
     @user = User.new
   end
+  
   def create
     @user = User.new(params[:user])
     if @user.save
@@ -12,6 +14,21 @@ class UsersController < ApplicationController
       redirect_to @user
     else
       render 'new'
+    end
+  end
+  
+  def edit
+    @user=User.find(params[:id])
+  end
+  
+  def update
+    @user= User.find(params[:id])
+    if @user.update_attributes(params[:user])
+      flash[:success]="Profile Updated"
+      sign_in @user
+      redirect_to @user
+    else
+      render "edit"
     end
   end
 end

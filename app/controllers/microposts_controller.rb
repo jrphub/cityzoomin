5a53f1b1e30601356136c6af8b4b923791816ab5
Jyoti Ranjan Pattnaik <jyotiranjanpattnaik@gmail.com>
2012-05-25 03:21:37 +0530

    location name is made unique

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -8,12 +8,16 @@ class MicropostsController < ApplicationController
   end
   
   def create
-    @location=Location.new({:name => params[:location][:name], :city => params[:location][:city],
+    @new_location=Location.find_by_name(params[:location][:name])
+    if @new_location
+       params[:micropost][:location_id] = @new_location.id
+    else
+       @location=Location.create({:name => params[:location][:name], :city => params[:location][:city],
               :state => params[:location][:state], :country => params[:location][:country]})
+       params[:micropost][:location_id] = @location.id
+    end
     
-    if @location.save
-      params[:micropost][:location_id] = @location.id
-    end          
+    
     @micropost=current_user.microposts.build({:content=>params[:micropost][:content],
                :location_id=>params[:micropost][:location_id],:title=>params[:micropost][:title],
                :category=>params[:micropost][:category]})

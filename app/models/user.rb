59343420d41a6c7073508a24e42851cf318c6c91
Jyoti Ranjan Pattnaik <jyotiranjanpattnaik@gmail.com>
2012-05-29 02:54:58 +0530

    name-username bug fixed

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,14 +12,14 @@
 #
 
 class User < ActiveRecord::Base
-  attr_accessible :name, :email, :password, :password_confirmation
+  attr_accessible :username, :email, :password, :password_confirmation
   has_secure_password
   has_many :microposts
   
   before_save { |user| user.email = email.downcase }
   before_save :create_remember_token
   
-  validates :name, presence: true, length: { maximum: 50 }
+  validates :username, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence:   true,
                     format:     { with: VALID_EMAIL_REGEX },

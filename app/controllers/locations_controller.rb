28d71b76ccea7f677c729fa59962723670282f97
Jyoti Ranjan Pattnaik <jyotiranjanpattnaik@gmail.com>
2012-05-30 13:39:19 +0530

    address links in post view page is working

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,10 +1,33 @@
 class LocationsController < ApplicationController
   def show
-    @allposts=Micropost.joins(:user)
-    .joins('INNER JOIN locations ON microposts.location_id = locations.id')
-    .select("microposts.id,content, location_id, title, category,microposts.created_at, microposts.updated_at, 
-    user_id, locations.name as locname, city, state, country, username, email").where(:location_id=>params[:id])
-    .paginate(page: params[:page],per_page:3)
-    @locname=@allposts.first.locname
+    if (params[:city])
+      @allposts=Micropost.joins(:user)
+      .joins('INNER JOIN locations ON microposts.location_id = locations.id')
+      .select("microposts.id,content, location_id, title, category,microposts.created_at, microposts.updated_at, 
+      user_id, locations.name as locname, city, state, country, username, email").where("city='#{params[:city]}'")
+      .paginate(page: params[:page],per_page:3)
+      @locname=@allposts.first.city
+    elsif (params[:state])
+      @allposts=Micropost.joins(:user)
+      .joins('INNER JOIN locations ON microposts.location_id = locations.id')
+      .select("microposts.id,content, location_id, title, category,microposts.created_at, microposts.updated_at, 
+      user_id, locations.name as locname, city, state, country, username, email").where("state='#{params[:state]}'")
+      .paginate(page: params[:page],per_page:3)
+      @locname=@allposts.first.state
+    elsif (params[:country])
+      @allposts=Micropost.joins(:user)
+      .joins('INNER JOIN locations ON microposts.location_id = locations.id')
+      .select("microposts.id,content, location_id, title, category,microposts.created_at, microposts.updated_at, 
+      user_id, locations.name as locname, city, state, country, username, email").where("country='#{params[:country]}'")
+      .paginate(page: params[:page],per_page:3)
+      @locname=@allposts.first.country
+    elsif(params[:id] != 'show')
+      @allposts=Micropost.joins(:user)
+      .joins('INNER JOIN locations ON microposts.location_id = locations.id')
+      .select("microposts.id,content, location_id, title, category,microposts.created_at, microposts.updated_at, 
+      user_id, locations.name as locname, city, state, country, username, email").where(:location_id=>params[:id])
+      .paginate(page: params[:page],per_page:3)
+      @locname=@allposts.first.locname
+    end
   end
 end

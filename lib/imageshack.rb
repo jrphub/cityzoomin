592c8c911742212524e7a662e25faa5ce9e7b0df
Amitav Mohanty <amitavmohanty01@gmail.com>
2012-06-13 22:27:58 +0530

    more xml details at put on to command line and more clarity into nokogiri parsing is shown

--- lib/imageshack.rb
+++ lib/imageshack.rb
@@ -12,5 +12,9 @@ class ImageShack
     response = CGI::unescapeHTML(response)
     doc = Nokogiri::XML(response)
     puts doc.root.elements.last.elements.first.children.first.text if doc.root.elements.last.elements.first.name = "image_link" and doc.root.elements.last.name == "links"
+    puts "rating = #{doc.root.elements.first.children[1].children.text.to_i}"
+    puts "width = #{doc.root.elements[2].children[1].children.text.to_i}"
+    puts "height = #{doc.root.elements[2].children[3].children.text.to_i}"
+    puts "#{doc.root.elements[5].name} = #{doc.root.elements[5].children.text}"
   end
 end

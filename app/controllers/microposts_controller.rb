51b36dff30cd4f1aefda1d21284a79f0572b95a8
Jyoti Ranjan Pattnaik <jyotiranjanpattnaik@gmail.com>
2012-06-16 04:02:18 +0530

    delete post via ajax

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -64,8 +64,13 @@ class MicropostsController < ApplicationController
   end
   
   def destroy
-    @micropost.destroy
-    redirect_to(:back, :notice=>"Successfully deleted")
+    @post = Micropost.find(params[:id])
+    @post.destroy
+    respond_to do |format|
+      format.html { redirect_to :back, :notice=>"Post is deleted successfully" }
+      format.json { head :no_content }
+      format.js #added
+    end
   end
   
   def vote_up

fcbfcb1a206cd7e9287db2a1a5bd263d1b18f3d8
Amitav Mohanty <dknight@fool.man.machine>
2012-06-16 00:12:29 +0530

    test on heroku

--- lib/imageshack.rb
+++ lib/imageshack.rb
@@ -7,7 +7,7 @@ class ImageShack
     post_data['key'] = "#{ENV[IMAGESHACK_KEY]}"
     post_data['a_user_name'] =  "#{ENV[IMAGESHACK_UID]}"
     post_data['public'] = "no"
-    post_data['fileupload'] = File.new("#{Rails.root}/assets/images/image_wall", "rb")
+    post_data['fileupload'] = File.new("#{Rails.root}/assets/images/bg.png", "rb")
     response = RestClient.post('http://imageshack.us/upload_api.php', post_data)
     response = CGI::unescapeHTML(response)
     doc = Nokogiri::XML(response)
